2587e9fbda59f52f474c806800c42cdb190e142a
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-13 22:57:53 +0430

    EcommerceFlaskProject

---
+++ site_module/models.py
@@ -0,0 +1,50 @@
+from extensions import db
+
+
+class SiteSetting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    site_name = db.Column(db.String(100), nullable=False)
+    url_name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=True)
+    fax = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=True)
+    copy_right = db.Column(db.String(100), nullable=False)
+    about_us_text = db.Column(db.Text, nullable=False)
+    site_logo = db.Column(db.String(200), nullable=False)  # مسیر فایل آپلود شده
+    is_main_setting = db.Column(db.Boolean, default=False)
+
+    def __str__(self):
+        return self.site_name
+
+
+class FooterLinkBox(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class FooterLink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(400), nullable=False)
+    footer_link_box_id = db.Column(db.Integer, db.ForeignKey('footer_link_boxes.id'), nullable=False)
+    footer_link_box = db.relationship('FooterLinkBox', backref=db.backref('footer_links', lazy=True))
+
+    def __str__(self):
+        return self.title
+
+
+class Slider(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    url = db.Column(db.String(400), nullable=False)
+    url_title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    image = db.Column(db.String(200), nullable=False)
+
+    def __str__(self):
+        return self.title
