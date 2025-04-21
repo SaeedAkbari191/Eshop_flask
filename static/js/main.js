cda701329bfab2095beeeeebd0cc23787b3f3cc3
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-21 17:34:00 +0430

    EcommerceFlaskProject

--- static/js/main.js
+++ static/js/main.js
@@ -132,3 +132,7 @@ function toggleExclusive(element) {
         arrow.classList.add('rotate');
     }
 }
+
+setTimeout(() => {
+    document.getElementById('alertBox').classList.add('alert-hidden');
+}, 5000);
