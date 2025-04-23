3702ab92940d1f7b3e8e35ec2101bd34f0fbac08
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-23 12:25:06 +0430

    EcommerceFlaskProject

--- home_module/views.py
+++ home_module/views.py
@@ -1,5 +1,8 @@
-import jinja2
 from flask import Blueprint, render_template
+from flask import Blueprint, render_template
+from sqlalchemy.orm import subqueryload
+
+from product_module.models import ProductCategory
 from site_module.models import Slider, SiteSetting
 
 views = Blueprint('views', __name__, template_folder='templates')
@@ -8,7 +11,9 @@ views = Blueprint('views', __name__, template_folder='templates')
 @views.route('/', methods=['GET', 'POST'])
 def home():
     slider = Slider.query.filter_by(is_active=True)
-    return render_template('home_module/index_page.html', sliders=slider, )
+    main_categories = ProductCategory.query.options(subqueryload(ProductCategory.parent)).filter_by(is_active=True,
+                                                                                                    parent_id=None).all()
+    return render_template('home_module/index_page.html', sliders=slider, main_categories=main_categories)
 
 
 @views.route('/about-us')
